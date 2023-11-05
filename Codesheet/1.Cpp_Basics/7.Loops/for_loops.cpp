bd641d0acb64e7c120a68063af3bcc123d022993
Chitresh-code <gychitresh1290@gmail.com>
2023-11-05 13:25:05 +0530

    Learned basics of Loops

---
+++ Codesheet/1.Cpp_Basics/7.Loops/for_loops.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Let's print numbers from 1 to 10
+    cout << 1 << endl;
+    cout << 2 << endl;
+    cout << 3 << endl;
+    cout << 4 << endl;
+    cout << 5 << endl;
+    cout << 6 << endl;
+    cout << 7 << endl;
+    cout << 8 << endl;
+    cout << 9 << endl;
+    cout << 10 << endl;
+    // This is a very tedious task. We can use loops to do this task easily.
+    // Using for loop
+    for(int i = 1; i <= 10; i++) {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
---
+++ Codesheet/1.Cpp_Basics/7.Loops/while_loops.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Now we will see while loop
+    // while loop is used when we don't know the number of iterations
+    // Here is how it is used
+    int i = 1;
+    while(i <= 10) {
+        cout << i << endl;
+        i++;
+    }
+    // This will print numbers from 1 to 10
+    
+    return 0;
+}
