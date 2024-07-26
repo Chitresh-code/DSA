359a0608ee33430171f6aab810c37f40d855ecc4
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-07-26 11:44:15 +0530

    Created q7.cpp

--- Assignments/Assignment 10/q7.cpp
+++ Assignments/Assignment 10/q7.cpp
@@ -9,8 +9,30 @@ int main() {
     int* ptr;
     ptr = (int *)malloc(n * sizeof(int));
     int arr[n], even_arr[n], odd_arr[n];
+    int even_count = 0, odd_count = 0;
     for (int i = 0;i < n;i++) {
         scanf("%d", &arr[i]);
     }
-    int 
+    
+    for (int i = 0;i < n;i++) {
+        if (arr[i] % 2 == 0) {
+            even_arr[even_count] = arr[i];
+            even_count++;
+        } else {
+            odd_arr[odd_count] = arr[i];
+            odd_count++;
+        }
+    }
+    
+    printf("Elements in the even array:\n");
+    for(int i = 0;i < even_count;i++) {
+        printf("%d ", even_arr[i]);
+    }
+
+    printf("\nElements in the odd array:\n");
+    for(int i = 0;i < odd_count;i++) {
+        printf("%d ", odd_arr[i]);
+    }
+
+    free(ptr);
 }
---
+++ Assignments/Assignment 10/q8.cpp
@@ -0,0 +1,7 @@
+// Write a C program to search an element in an array.
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+    
+}
