7b425711c9a26090e35bfcaf0c412941afc8e94f
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-07-26 11:30:28 +0530

    Created q6.cpp

---
+++ Assignments/Assignment 10/q6.cpp
@@ -0,0 +1,26 @@
+// Write a C program to find reverse of an array. 
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int* ptr;
+    ptr = (int *)malloc(n * sizeof(int));
+    int arr[n];
+    for (int i = 0;i < n;i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0;i < n/2;i++) {
+        arr[i] = arr[i] + arr[n-i-1];
+        arr[n-i-1] = arr[i] - arr[n-i-1];
+        arr[i] = arr[i] - arr[n-i-1];
+    }
+
+    printf("Reverse of the input array:\n");
+    for(int i = 0;i < n;i++) {
+        printf("%d ", arr[i]);
+    }
+}
