d3abfefd318a3f0034faf7e6920611ef8fb05a4e
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-07-26 11:48:01 +0530

    Created q8.cpp

--- Assignments/Assignment 10/q8.cpp
+++ Assignments/Assignment 10/q8.cpp
@@ -3,5 +3,26 @@
 #include <cstdlib>
 
 int main() {
-    
+    int n, ele, check = 0;
+    scanf("%d", &n);
+
+    int* ptr;
+    ptr = (int *)malloc(n * sizeof(int));
+    int arr[n], even_arr[n], odd_arr[n];
+    int even_count = 0, odd_count = 0;
+    for (int i = 0;i < n;i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &ele);
+    for (int i = 0;i < n;i++) {
+        if (ele == arr[i])
+            check++;
+    }
+
+    if (check != 0)
+        printf("%d is in the array", ele);
+    else
+        
+        printf("%d is not in the array", ele);
 }
