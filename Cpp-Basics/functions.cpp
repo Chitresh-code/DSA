53463de96a251f40649c25eeacb4968c1d53478c
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-03 16:40:35 +0530

    Learned functions

---
+++ Cpp-Basics/functions.cpp
@@ -0,0 +1,45 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // non-parameterised void function -> does not take any parameters and does not return anything
+// void printHello() {
+//     cout << "Hello, World!" << endl;
+// }
+
+// // parameterised void function -> takes parameters and does not return anything
+// void printName(string name) {
+//     cout << "Welcome " << name << endl;
+// }
+
+// // non-parameterised non-void function ->  does not take any parameters and return a value
+// int getRandomNumber() {
+//     return rand() % 100;
+// }
+
+// // parameterised non-void function ->  takes parameters and return a value
+// int addTwoNos(int a, int b) {
+//     return a + b;
+// }
+
+// int main() {
+//     string name;
+//     getline(cin, name);
+
+//     // non-parameterised void functio
+//     printHello();
+
+//     // parameterised void function
+//     printName(name);
+
+//     // non-parameterised non-void function
+//     int random = getRandomNumber();
+//     cout << "Random number: " << random << endl;
+
+//     // parameterised non-void function
+//     int a, b;
+//     cin >> a >> b;
+//     int res = addTwoNos(a, b);
+//     cout << "Sum of " << a << " and " << b << " is: " << res;
+
+//     return 0;
+// }
