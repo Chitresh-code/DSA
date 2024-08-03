344653010ae7f7f01936fc16ea005fcf0ac16a3f
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-03 19:23:48 +0530

    learned list

---
+++ STL/list.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // list<int> li;
+
+    // li.push_back(2);
+    // li.emplace_back(4);
+
+    // li.push_front(5);
+    // li.emplace_front(10);
+
+    // for(auto it : li) {
+    //     cout << it << " ";
+    // }
+
+    // rest of the functions are same as vector
+
+    return 0;
+}
