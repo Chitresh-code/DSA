5e81084812400e88132e1ab38230fe40140fccf3
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-03 23:51:54 +0530

    learned deque

---
+++ STL/deque.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // deque<int> dq;
+
+    // dq.push_back(2);
+    // dq.emplace_back(4);
+
+    // dq.push_front(5);
+    // dq.emplace_front(10);
+
+    // dq.pop_back();
+    // dq.pop_front();
+
+    // cout << dq.back() << endl;
+    // cout << dq.front() << endl;
+
+    // for(auto it : dq) {
+    //     cout << it << " ";
+    // }
+
+    // rest of the functions are same as vector
+
+    return 0;
+}
