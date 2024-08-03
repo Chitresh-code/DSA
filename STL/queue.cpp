b88d273fc09e0dc7b03844ba4e857cdabcf35d47
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-04 00:07:14 +0530

    learned queue

---
+++ STL/queue.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    queue<int> q;
+    q.push(1); // {1}
+    q.push(2); // {1, 2,}
+    q.push(3); // {1, 2, 3}
+    q.emplace(5); // {1, 2, 3, 5}
+
+    cout << q.back() << endl;
+    cout << q.front() << endl;
+
+    q.back() += 5; // 10
+    cout << q.back() << endl;
+
+    q.pop();
+    cout << q.front() << endl;
+
+    return 0;
+}
