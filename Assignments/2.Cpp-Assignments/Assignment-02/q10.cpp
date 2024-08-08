ee012738c7cda403f53633a50124df690eb17815
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-08 10:27:06 +0530

    q10

--- Assignments/2.Cpp-Assignments/Assignment-02/q10.cpp
+++ Assignments/2.Cpp-Assignments/Assignment-02/q10.cpp
@@ -1,3 +1,30 @@
 // Write a program to read time in hh:mm:ss and display answer in only seconds.
 #include<bits/stdc++.h>
 using namespace std;
+
+class Seconds {
+private:
+    int hours, minutes, seconds;
+
+public:
+    void input();
+    void output();
+};
+
+void Seconds :: input() {
+   cin >> hours;
+   cin >> minutes;
+   cin >> seconds; 
+}
+
+void Seconds :: output() {
+    minutes += hours * 60; 
+    seconds += minutes * 60;
+    cout << "Seconds: " << seconds;
+}
+
+int main() {
+    Seconds sec;
+    sec.input();
+    sec.output();
+}
