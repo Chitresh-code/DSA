b8a0b7e3cd0c0b616d02c30a82dd50fc35c1b9b5
Chitresh Gyanani <gychitresh1290@gmail.com>
2024-08-08 12:31:02 +0530

    q5

--- Assignments/2.Cpp-Assignments/Assignment-03/q5.cpp
+++ Assignments/2.Cpp-Assignments/Assignment-03/q5.cpp
@@ -1,35 +1,62 @@
-// Create a class “Mobile” with attributes: brand, price, color, width, height. 
-// Use constructor to set default values of these attributes. 
-// Write function to display details of all attributes
+// Create a class “Mobile” with attributes: brand, price, color. Enter detail of five different mobile. (Using Array of object).
+// Display total number of mobile having price greater than 5000.
+// Display Brand, Price and color for all mobiles for price range 1000 to 10000
 #include<bits/stdc++.h>
 using namespace std;
 
 class Mobile {
     private:
-        float price, width, height;
+        float price;
         string brand, color;
     
     public:
-        Mobile(float p, float w, float h, string b, string c) {
-            this->brand = b;
+        Mobile(float p, string b, string c) {
+            brand = b;
             price = p;
             color = c;
-            width = w;
-            height = h; 
         }
         void display();
+        float getPrice();
 };
 
 void Mobile :: display() {
     cout << "Brand: " << brand << endl;
     cout << "Price: " << price << endl;
     cout << "Color: " << color << endl;
-    cout << "Width: " << width << endl;
-    cout << "Height: " << height << endl;
+}
+
+float Mobile :: getPrice() {
+    return price;
 }
 
 int main() {
-    vector<Mobile> mob(5);
+    vector<Mobile> mob;
+    float price;
+    string brand, color;
 
+    for (int i = 0; i < 5; ++i) {
+        getline(cin, brand);
+        cin >> price;
+        cin.ignore();
+        getline(cin, color);
+        mob.push_back(Mobile(price, brand, color));
+    }
 
+    int count = 0;
+    for (auto &i : mob) {
+        if (i.getPrice() > 5000) {
+            count++;
+        }
+    }
+    cout << "Total mobiles having price greater than 5000: " << count << endl;
+
+    cout << "Mobiles in the price range of 1000 to 10000:" << endl;
+    for (auto &i : mob) {
+        if (i.getPrice() >= 1000 && i.getPrice() <= 10000) {
+            i.display();
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
